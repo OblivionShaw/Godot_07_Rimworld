e5663ccb3d33b370f81d2401cfb5a2d0ca9c4685
OblivionShaw <sh960208@gmail.com>
2024-06-13 19:51:34 +0800

    feat:update

--- MagicSurvivors/Terrain.gd
+++ MagicSurvivors/Terrain.gd
@@ -4,6 +4,16 @@ extends TileMap
 @export var generateTerrain : bool
 @export var clearTerrain : bool
 
+@export var mapWidth : int
+@export var mapHeight : int
+@export var terrainSeed : int
+
+@export var grassThreshold : float
+@export var grass2Threshold : float
+@export var dirtThreshold : float
+@export var rockThreshold : float
+
+
 func _ready():
 	pass
 
@@ -19,8 +29,25 @@ func _process(delta):
 func GenerateTerrain():
 	print("Generating Terrain")
 	
+	var noise = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_CELLULAR
+	
 	var rng = RandomNumberGenerator.new()
 	
-	for x in range(16):
-		for y in range(16):
-			set_cell(0,Vector2i(x,y),0,Vector2i(randi_range(0,5),0))
+	if terrainSeed == 0:
+		noise.seed = rng.randi()
+	else:
+		noise.seed = terrainSeed
+	
+	for x in range(mapWidth):
+		for y in range(mapHeight):
+			if noise.get_noise_2d(x,y) > grassThreshold:
+				set_cell(0,Vector2i(x,y),0,Vector2i(0,0))
+			elif noise.get_noise_2d(x,y) > grass2Threshold:
+				set_cell(0,Vector2i(x,y),0,Vector2i(1,0))
+			elif noise.get_noise_2d(x,y) > dirtThreshold:
+				set_cell(0,Vector2i(x,y),0,Vector2i(2,0))
+			elif noise.get_noise_2d(x,y) > rockThreshold:
+				set_cell(0,Vector2i(x,y),0,Vector2i(3,0))
+			else:
+				set_cell(0,Vector2i(x,y),0,Vector2i(0,1))
